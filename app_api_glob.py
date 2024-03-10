46276627852b64a6890656e5e9844a3b22ee75e1
Luciano <loberti@corebi.com.ar>
2024-03-10 11:34:54 -0300

    prueba instalacion librerias

--- app_api_glob.py
+++ app_api_glob.py
@@ -1,1 +1,9 @@
-.import pandas as pd
+#importo librerias
+import pandas as pd
+import numpy as np
+import os
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+
+serie = pd.Series([1,2,3,4])
+print(serie)
